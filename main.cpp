664c8ad1f513d4c14343dd69c6a8f73045cde606
LCampbell-UTM <lcampb66@skyhawks.utm.edu>
2026-01-21 11:33:26 -0600

    added the iterative number sum

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 
+int sum(int input)
+{
+  int total = 0;
+
+  for(int i=0;i<input+1;i++)
+    {
+      total+=i;
+    }
+
+  return total;
+}
+
+
 int main()
 {
 
   std::cout << "Hello world!" << std::endl;
+
+  std::cout << "\n";
+
+  std::cout << "Enter a number: ";
+
+  int num;
+
+  std::cin >> num;
+
+  int output = sum(num);
+  
+  std::cout << "From 1 to " << num <<", the sum iterative sum toal is: " << output << std::endl;
   
   return 0;
 }
